1fa14aa2582d09512b3f9dd8665feadb97ec474f
Sean Kugele <seankugele@gmail.com>
2017-02-13 12:43:02 +0000

    Adding configs directory and "stub" sensory_memory and sensory_motor_memory code and agent config.

---
+++ src/sensory_memory.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+from PIL import Image
+from StringIO import StringIO
+from sys import argv
+
+import lidapy
+from lidapy import LIDAThread, Config
+from sensor_msgs.msg import CompressedImage
+
+image_topic = lidapy.Topic('image_topic', msg_type=CompressedImage)
+
+
+def receive_image():
+    msg = image_topic.receive()  # type: CompressedImage
+    if msg:
+        # TODO: Create the pixel layer from this image's data
+        image = Image.open(StringIO(msg.data))
+
+
+# Initialize the lidapy framework
+lidapy.init(config=Config(argv[1]), process_name='sensory_memory')
+LIDAThread(name='listener', callback=receive_image).start()
---
+++ src/sensory_motor_memory.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from random import choice
+from sys import argv
+
+import lidapy
+from lidapy import LIDAThread, Config
+
+action_topic = lidapy.Topic('actions')
+
+actions = ['LEFT', 'RIGHT', 'DOWN', 'ROTATE']
+
+
+def send_action():
+    action = choice(actions)
+    action_topic.send(action)
+
+
+# Initialize the lidapy framework
+lidapy.init(config=Config(argv[1]), process_name='sensory_motor_memory')
+LIDAThread(name='listener', callback=send_action).start()
