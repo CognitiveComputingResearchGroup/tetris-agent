22cb2c133fb456ef2d991c3230a599bf51a120a5
Sean Kugele <seankugele@gmail.com>
2017-02-13 13:16:40 +0000

    Updated tetris environment to receive agent actions.

--- env/src/game/main.py
+++ env/src/game/main.py
@@ -44,7 +44,8 @@ class main(object):
 
         lidapy.init(process_name='Environment')
 
-        self.image_topic = lidapy.Topic('image_topic', msg_type=CompressedImage)
+        self.image_topic = lidapy.Topic('images', msg_type=CompressedImage, queue_size=1)
+        self.action_topic = lidapy.Topic('actions', queue_size=1)
 
         self.mainloop()
 
@@ -58,54 +59,60 @@ class main(object):
         self.timer = pygame.time.Clock()
 
         while self.run == True:
-            ##debug.log.write('\n\nFrame:\n')
+
             if self.tochangeto != '':
                 self.submenu.destroy()
                 self.submenu = self.tochangeto
                 self.submenu.draw_all(self.screen)
                 pygame.display.flip()
                 self.tochangeto = ''
-            # events
-            pygame.event.pump()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.run = False
-                elif event.type == ACTIVEEVENT:
-                    if event.gain == 1:
-                        # our window has come to the front after being hidden, refresh
-                        self.submenu.draw_all(self.screen)
-                        pygame.display.flip()
-                    elif event.state == 2:
-                        # our window is hidden so wait for the next event
-                        pygame.event.post(pygame.event.wait())
-                elif event.type == pygame.MOUSEMOTION:
-                    self.submenu.mouse_motion(event)
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    self.submenu.mouse_button_down(event)
-                elif event.type == pygame.MOUSEBUTTONUP:
-                    self.submenu.mouse_button_up(event)
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == K_ESCAPE:
+
+            # agent actions
+            agent_action = self.action_topic.receive(timeout=0.01)
+            if agent_action:
+                self.process_agent_action(agent_action)
+            else:
+                # events
+                pygame.event.pump()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
                         self.run = False
-                    elif event.key in LEFT_KEYS:
-                        self.submenu.left_pressed()
-                    elif event.key in RIGHT_KEYS:
-                        self.submenu.right_pressed()
-                    elif event.key in UP_KEYS:
-                        self.submenu.up_pressed()
-                    elif event.key in DOWN_KEYS:
-                        self.submenu.down_pressed()
-                    else:
-                        self.submenu.key_press(event)
-                elif event.type == pygame.KEYUP:
-                    if event.key in LEFT_KEYS:
-                        self.submenu.left_unpressed()
-                    elif event.key in RIGHT_KEYS:
-                        self.submenu.right_unpressed()
-                    elif event.key in UP_KEYS:
-                        self.submenu.up_unpressed()
-                    elif event.key in DOWN_KEYS:
-                        self.submenu.down_unpressed()
+                    elif event.type == ACTIVEEVENT:
+                        if event.gain == 1:
+                            # our window has come to the front after being hidden, refresh
+                            self.submenu.draw_all(self.screen)
+                            pygame.display.flip()
+                        elif event.state == 2:
+                            # our window is hidden so wait for the next event
+                            pygame.event.post(pygame.event.wait())
+                    elif event.type == pygame.MOUSEMOTION:
+                        self.submenu.mouse_motion(event)
+                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                        self.submenu.mouse_button_down(event)
+                    elif event.type == pygame.MOUSEBUTTONUP:
+                        self.submenu.mouse_button_up(event)
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == K_ESCAPE:
+                            self.run = False
+                        elif event.key in LEFT_KEYS:
+                            self.submenu.left_pressed()
+                        elif event.key in RIGHT_KEYS:
+                            self.submenu.right_pressed()
+                        elif event.key in UP_KEYS:
+                            self.submenu.up_pressed()
+                        elif event.key in DOWN_KEYS:
+                            self.submenu.down_pressed()
+                        else:
+                            self.submenu.key_press(event)
+                    elif event.type == pygame.KEYUP:
+                        if event.key in LEFT_KEYS:
+                            self.submenu.left_unpressed()
+                        elif event.key in RIGHT_KEYS:
+                            self.submenu.right_unpressed()
+                        elif event.key in UP_KEYS:
+                            self.submenu.up_unpressed()
+                        elif event.key in DOWN_KEYS:
+                            self.submenu.down_unpressed()
             # update
             self.submenu.update()
             # display
@@ -128,3 +135,25 @@ class main(object):
 
         # end of game
         pygame.quit()
+
+
+    def process_agent_action(self, action):
+        if action:
+            action = action.data
+
+        if action == 'LEFT':
+            self.submenu.left_pressed()
+            self.submenu.update()
+            self.submenu.left_unpressed()
+        elif action == 'RIGHT':
+            self.submenu.right_pressed()
+            self.submenu.update()
+            self.submenu.right_unpressed()
+        elif action == 'ROTATE':
+            self.submenu.up_pressed()
+            self.submenu.update()
+            self.submenu.up_unpressed()
+        elif action == 'DOWN':
+            self.submenu.down_pressed()
+            self.submenu.update()
+            self.submenu.down_unpressed()
